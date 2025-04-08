351822fefc1876ac1c33d0761be5f3416f374494
kyawkyawlin <kkyawlin.connetis@gmail.com>
2025-04-08 23:03:40 +0700

    filter

---
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Http\Resources\V1\CustomerResource;
+use App\Models\Customer;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request);
+        if (count($queryItems)) {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
+        return new CustomerCollection(Customer::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCustomerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return new CustomerResource($customer);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ database/migrations/test.php
@@ -0,0 +1,169 @@
+<div class="subBlog">
+								<h2 class="mainTitle">休職者<span class="subBlue">管理</span>システム(週一報告システム)</span></h2>
+								<div class="commonBlog">
+									<h3 class="blueTitle">高尾メソッドにもとづく療養・復帰準備状況報告書の提出､<br>受領書の発行のやり取りを、SaaS化で業務効率化<span class="subGray">休職者側も人事労務担当者側も、最適化されたインターフェースでカンタン操作</span></h3>
+									<div class="infoList">
+										<p class="blueText">休職者管理システム（週一報告システム）とは…?</p>
+										<ul class="firstList">
+											<li>復職プログラム「高尾メソッド」に基づく、手書き・紙ベースでのやり取りをSaaS化。</li>
+											<li>企業や自治体の休職者と上司や人事労務担当者の双方がシステム上で報告書提出・受領書発行のやり取りを行う。</li>
+											<li>管理ページには､それまでの報告書と受領書の履歴が残ることで､担当者の交代の際にも引き継ぎが容易。</li>
+										</ul>
+										<ul class="secList">
+											<li>医療的アプローチではなく､業務的アプローチを用いることで､休職者対応そのものから人事総務担当者が自信を持って行えるようにするだけではなく､復職後の予後を大幅に改善する（再療養減少など）ことのできる「高尾メソッド」｡</li>
+											<li>休職者対応の中でも､従来のプログラムと対比した場合に､非常に特徴的でもある､週1回の休職者からの「療養・復帰準備状況報告」および､それに対する会社側からの受領書の発行のやりとり｡</li>
+											<li>このやりとりにより､休職者は復職基準を常に意識しながら､しっかりとした復帰準備を自ら主体的に考え､取り組んでいくことで､復職後に再療養に至るなどの負の事象の発生が大幅に減少するだけでなく､なにより復職後の業務パフォーマンスが期待できる｡</li>
+											<li>このキモとも言える部分が､週1回の報告とそれに対するフィードバックであることは言わずもがなではあったものの､一方で企業規模が大きくなり､同時の休職者数が数十名を超えるようになると､やるべきだとわかっていても､作業量の点から取り組むことができなかった点について､システムが支援､解決する｡</li>
+										</ul>
+									</div>
+
+                                                                        <div class="functionBlog pupleBlog col">
+										<div class="functionText">
+											<p class="blueText"><span>人事労務担当者向け機能</span></p>
+											<ul class="functionList">
+												<li><span>一覧表示で休職者を一元管理</span></li>
+												<li><span>報告書へのコメントを定型文で効率的に</span></li>
+												<li><span>休職者毎のメモ、エクスポートなど、細かい機能も充実</span></li>
+											</ul>
+											<div class="videoBlog">
+                                                                                           <!-- Video link -->
+
+<a href="javascript:void(0);" class="" id="videoLink" oncontextmenu="return false;">
+  <figure class="movieBtn">
+    <img src="assets/img/common/youtube_icon.png" alt="人事労務担当者向け操作イメージはこちらから">
+    <figcaption>人事労務担当者向け操作イメージはこちらから</figcaption>
+  </figure>
+</a>
+
+<!-- Video Modal HTML -->
+<div id="videoModal" style="display:none; position:fixed; z-index:9999; left:0; top:0; width:100%; height:100%; overflow:auto; background-color:rgba(0,0,0,0.8);">
+  <!-- Modal content -->
+  <div style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%); width:80%;">
+    <span id="closeVideoModal" style="color:white; float:right; font-size:28px; font-weight:bold; cursor:pointer; margin-bottom:10px;">×</span>
+    <div style="clear:both;">
+      <video id="modalVideo" width="100%" controls controlsList="nodownload" oncontextmenu="return false;"></video>
+    </div>
+  </div>
+</div>
+
+<script>
+// Wait for DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the modal
+  var modal = document.getElementById('videoModal');
+
+  // Get the video element
+  var video = document.getElementById('modalVideo');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById('videoLink');
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementById('closeVideoModal');
+
+  // Video source
+  var videoSrc = 'https://dev.doctoral.co.jp/wp-content/themes/EBP/assets/video/動画素材_人事労務ユーザー.mp4';
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+    video.src = videoSrc;
+
+    try {
+      video.play();
+    } catch (err) {
+      console.log('Error playing video:', err);
+    }
+
+    return false;
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    video.pause();
+    video.src = "";
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal content, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      video.pause();
+      video.src = "";
+      modal.style.display = "none";
+    }
+  }
+});
+</script>
+											</div>
+										</div>
+										<figure class="functionImg">
+											<img src="assets/img/service/houkoku_img3.png" alt="人事労務担当者向け機能">
+										</figure>
+									</div>
+									<div class="functionBlog col">
+										<div class="functionText">
+											<p class="blueText"><span>休職者向け機能</span></p>
+											<ul class="functionList">
+												<li><span>スマートフォン向けUI</span></li>
+												<li><span>報告書を手書きで作成のうえ、写真でアップロード</span></li>
+												<li><span>チャット画面で見やすいフィードバック</span></li>
+											</ul>
+											<div class="videoBlog">
+												<a href="assets/video/動画素材_休職者ユーザー.mp4" class="video blueLink" controlsList="nodownload" oncontextmenu="return false;" onclick="return false;" >
+													<figure class="movieBtn">
+														<img src="assets/img/common/youtube_icon.png" alt="休職者向け操作イメージはこちらから">
+														<figcaption>休職者向け操作イメージはこちらから</figcaption>
+													</figure>
+												</a>
+											</div>
+										</div>
+										<figure class="functionImg">
+											<img src="assets/img/service/houkoku_img1.png" alt="休職者向け機能">
+										</figure>
+									</div>
+
+								</div>
+								<div class="takaoMethodDescription">
+									<div class="functionText linkBlog">
+										<p class="blueText">高尾メソッドについて</p>
+									  <p>高尾メソッドについては、下記の記事をご参照ください。</p>
+									</div>
+
+									<div class="linkBlog infoList">
+										<p class="secTitle">安全と健康フォーラム</p>
+									  <ul class="secList">
+									    <li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2022/08/%E5%9C%B0%E6%96%B9%E5%85%AC%E5%8B%99%E5%93%A1-%E5%AE%89%E5%85%A8%E3%81%A8%E5%81%A5%E5%BA%B7%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A9%E3%83%A0_122%E5%8F%B7_%E9%80%A3%E8%BC%893.pdf" class="blueLink" target="_blank" rel="noopener">[第122号]</a></li>
+									    <li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2022/11/%E5%9C%B0%E6%96%B9%E5%85%AC%E5%8B%99%E5%93%A1-%E5%AE%89%E5%85%A8%E3%81%A8%E5%81%A5%E5%BA%B7%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A9%E3%83%A0_123%E5%8F%B7_26-27%E9%80%A3%E8%BC%893.pdf" class="blueLink" target="_blank" rel="noopener">[第123号]</a></li>
+									    <li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2023/03/forum_vol.124_26-27_ss3.pdf" class="blueLink" target="_blank" rel="noopener">[第124号]</a></li>
+									    <li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2023/07/%E5%9C%B0%E6%96%B9%E5%85%AC%E5%8B%99%E5%93%A1-%E5%AE%89%E5%85%A8%E3%81%A8%E5%81%A5%E5%BA%B7%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A9%E3%83%A0_125%E5%8F%B7_26-27%E9%80%A3%E8%BC%893.pdf" class="blueLink" target="_blank" rel="noopener">[第125号]</a></li>
+									    <li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2023/11/forum_126_rensai3.pdf" class="blueLink" target="_blank" rel="noopener">[第126号]</a></li>
+									    <li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2024/03/127_p26-27_rensai3.pdf" class="blueLink" target="_blank" rel="noopener">[第127号]</a></li>
+											<li><a href="https://www.jalsha.or.jp/wordpress/wp-content/uploads/2024/07/128_p26-27_rensai3.pdf" class="blueLink" target="_blank" rel="noopener">[第128号]</a></li>
+									  </ul>
+									</div>
+
+									<div class="linkBlog infoList">
+										<p class="secTitle">ビジネス・レーバー・トレンド</p>
+									  <ul class="secList">
+									    <li><a href="https://www.jil.go.jp/kokunai/blt/backnumber/2011/07/018-021.pdf" class="blueLink" target="_blank" rel="noopener">[2011年7月号]</a></li>
+									    <li><a href="https://www.jil.go.jp/kokunai/blt/backnumber/2012/05/024-031.pdf" class="blueLink" target="_blank" rel="noopener">[2012年5月号]</a></li>
+									    <li><a href="https://www.jil.go.jp/kokunai/blt/backnumber/2014/09/036-041.pdf" class="blueLink" target="_blank" rel="noopener">[2014年9月号]</a></li>
+									    <li><a href="https://www.jil.go.jp/kokunai/blt/backnumber/2015/09/034-039.pdf" class="blueLink" target="_blank" rel="noopener">[2015年9月号]</a></li>
+									    <li><a href="https://www.jil.go.jp/kokunai/blt/backnumber/2019/01_02/034-043.pdf" class="blueLink" target="_blank" rel="noopener">[2019年1−2月号]</a></li>
+									    <li><a href="https://www.jil.go.jp/kokunai/blt/backnumber/2021/12/018-029.pdf" class="blueLink" target="_blank" rel="noopener">[2021年12月号]</a></li>
+									  </ul>
+									</div>
+
+									<div class="linkBlog">
+										<p class="secTitle"><a href="https://okayamas.johas.go.jp/wp-content/uploads/2021/02/2014%E3%83%A1%E3%83%B3%E3%82%BF%E3%83%AB%E3%83%98%E3%83%AB%E3%82%B9%E7%A0%94%E7%A9%B6%E4%BC%9A%E5%A0%B1%E5%91%8A%E6%9B%B8.pdf" class="blueLink" target="_blank" rel="noopener">メンタルヘルス研究会報告書</a></p>
+									</div>
+
+									<div class="linkBlog infoList">
+										<p class="secTitle">埼玉県春日部市における取り組み</p>
+									  <ul class="secList">
+									    <li><a href="https://www.chikousai.go.jp/boushi2/pdf/201302.pdf" class="blueLink" target="_blank" rel="noopener">[公務災害の現場から2013]</a></li>
+									  </ul>
+									</div>
+								</div>
+							</div>
